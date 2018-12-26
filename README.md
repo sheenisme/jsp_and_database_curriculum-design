5b2dc43a222895ac3a290c20a81fd4d3b842573b
sheenisme <35988393+sheenisme@users.noreply.github.com>
2018-12-26 19:29:58 +0800

    test github and eclipse

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # jsp_and_database_curriculum-design
+# 这个是我的jsp和数据库课程设计的作业，仅供参考！
---
+++ src/weiyinetclient/Mytest.java
@@ -0,0 +1,9 @@
+package weiyinetclient;
+/**
+ * 测试GitHub是否可以显示中文
+ * @author 广辉
+ *
+ */
+public class Mytest {
+
+}
